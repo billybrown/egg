8d6ee9484d07b3ed208d4e633a151d2ae804a1cd
Bill Brown <bill@echo.co>
2015-08-11 17:34:46 -0500

    updated some commenting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,34 @@
+/*
+
+    First-time Grunt installation:
+    -------------------
+    npm install -g grunt-cli
+    npm install -g grunt-init
+    Not working? Weird errors? Try Grunt's getting started guide: http://gruntjs.com/getting-started
+
+    Simple Dependency Install:
+    --------------------------
+    npm start (from the same root directory as the `package.json` file) (this runs both 'npm install' and 'bower install')
+
+    Tasks:
+    --------------------------
+    grunt (default is to watch both sass, js, and images)
+    grunt css (compile sass into css once)
+    grunt watch (you can also explicitly call the watch task)
+    grunt build (recompiled everything - sass, sprites, javascript, etc.)
+
+    All commands are detailed by running the following:
+    --------------------------
+    grunt --help
+
+    Node Version
+    --------------------------
+    There is a file called .nvmrc in the root of the theme that indicates what version of node we are using.
+    Everything should work even if you have a different version - but in case you are getting errors
+    and you suspect node - the version number in .nvmrc should work.
+
+*/
+
 'use strict';
 
 module.exports = function(grunt) {
