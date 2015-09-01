df09300db8ea4a2ace323d7cae8af1037214c887
Bill Brown <bill@echo.co>
2015-09-01 13:52:45 -0500

    restructured js

--- build/js/_readme.txt
+++
@@ -1,1 +0,0 @@
-// put your vendor js here
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,46 +3,44 @@
 jQuery( document ).ready( function( $ ) {
 	"use strict";
 
-	var Engine = {
-		
-		InitializePlugin : {
 
-			Fitvids : function() {
-				// this makes all videos responsive inside the main content area
-				$(".SiteMain").fitVids();
-			}
+	/////////////////////////////////////-+++-
+	// plugin initialization
 
-			// ChosenSelect : function() {
-			// 	// this makes all select elements with this wrapping class have the chosen style
-			// 	$(".u-select select").chosen();
-			// }	
-		},
+	// this makes all videos responsive inside the main content area
+	$(".SiteMain").fitVids();
 
-		UI: {
+	// this makes all select elements with this wrapping class have the chosen style
+	// 	$(".u-select select").chosen();
 
-			ClassTrigger : function(trigger, receiver, classer) {
-				// trigger the adding and removing of classes. Actual animation is handled by CSS.
-				$(trigger).click(function() {
-					$(receiver).toggleClass(classer);
-				});
-			}
 
-			// EqualizeHeights: function() {
-			// 	// using enquire to only trigger match height at a certain breakpoint
-			//  enquire.register("screen and (min-width:940px)", {
-			//		match : function() {
-			//			$('.CardColorGrid .Card').matchHeight();
-			//               }, unmatch : function() {
-			//                   $('.CardColorGrid .Card').matchHeight('remove');
-			//               }
-			//           }, true);
-			//       }
-			//	}
 
-		}
+	/////////////////////////////////////-+++-
+	// custom functions
+
+	var classTrigger = function(trigger, receiver, classer) {
+		// trigger the adding and removing of classes. Actual animation is handled by CSS.
+		$(trigger).click(function() {
+			$(receiver).toggleClass(classer);
+		});
 	};
 
-	Engine.InitializePlugin.Fitvids();
-	Engine.UI.ClassTrigger('#MobileMenu__close, #MobileMenu__open', 'html', 'active-MobileMenu');
+	// equalizeHeights: function() {
+	// 	// using enquire to only trigger match height at a certain breakpoint
+	//  enquire.register("screen and (min-width:940px)", {
+	//		match : function() {
+	//			$('.CardColorGrid .Card').matchHeight();
+	//               }, unmatch : function() {
+	//                   $('.CardColorGrid .Card').matchHeight('remove');
+	//               }
+	//           }, true);
+	//       }
+	//	}
+
+
+	/////////////////////////////////////-+++-
+	// call those custom functions
+
+	classTrigger('#MobileMenu__close, #MobileMenu__open', 'html', 'active-MobileMenu');
 
 });
